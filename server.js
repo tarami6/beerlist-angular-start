120f5168eae3c7ff8901c0297a91666ab096a4f4
Steven Winston <swinston100@hotmail.com>
2017-06-29 18:54:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(8000, function() {
-  console.log("Fullstack project. Listening on 8000.")
+  console.log("yo yo yo, on 8000!!")
 });
