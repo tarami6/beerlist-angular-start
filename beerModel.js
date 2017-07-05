300026415793a03b10734245b9a485bae1282d04
Rami Talisveiber <allindn@gmail.com>
2017-07-05 15:17:20 +0300

    backend

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,22 @@
+[{
+     name: '512 IPA',
+      style: 'IPA',
+       image_url: 'http://bit.ly/1XtmB4d',
+        abv: 5 },
+{ name: '512 Pecan Porter', style: 'Porter', image_url: 'http://bit.ly/1Vk5xj4', abv: 4 }]
 
+var mongoose = require('mongoose');
+
+// creating schema fot beer
+
+var beerSchema = new mongoose.Schema({
+    name:String,
+    style:String,
+    image_url:String,
+    abv:Number,
+    ratings:[]
+})
+
+var Beer = mongoose.model('Beer', beerSchema);
+
+module.exports = Beer;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+
+//Beer Db 
+mongoose.connect('mongodb://localhost/beers');
+var Beer = require('./beerModel.js');
 
 var app = express();
 
@@ -8,6 +13,72 @@ app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(8000, function() {
+// handler Func
+var handler = function(res,next){
+  return function(err, beer){
+    if(err){
+      return next(err);
+    } else {
+      res.send(beer);
+    }
+  }
+}
+
+// get beer from dataBade 'beers'
+
+app.get('/beers', function (req, res, next) {
+  Beer.find(handler(res,next));
+});
+
+// add beers to our database
+app.post('/beers', function (req, res, next) {
+  Beer.create(req.body, handler(res,next))
+})
+
+// deleting beer from db
+app.delete('/beers/:id', function (req, res, next) {
+  Beer.findByIdAndRemove(req.params.id, handler(res,next));
+});
+// updating the rate of the beer
+app.post('/beers/:id/:ratings', function (req, res, next) {
+
+  // code a suitable update object
+  // using req.body to retreive the new rating
+  var updateObject = { $push: { ratings: req.body.ratings } };
+  console.log(updateObject);
+  Beer.findByIdAndUpdate(req.params.id, updateObject, { new: true }, handler(res,next));
+});
+
+app.put('/beers/:id', function(req, res, next) {
+  console.log('update');
+  console.log(req.body);
+  Beer.findByIdAndUpdate(req.params.id, req.body, handler(res,next));
+});
+
+
+// middleware to handle errors
+// err handler to cath 404 and forward to main error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// main error handler
+// warning - not for use in production code!
+
+app.use(function (err, rew, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
+
+
+
+
+
+app.listen(8000, function () {
   console.log("yo yo yo, on 8000!!")
 });
